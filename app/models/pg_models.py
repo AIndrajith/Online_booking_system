5b1bf19d7ccbffbb400496b017be26ed04918683
AIndrajith <deegalla12378@usci.ruh.ac.lk>
2026-07-21 04:32:24 +0530

    complete remaining models in pg_models

--- app/models/pg_models.py
+++ app/models/pg_models.py
@@ -31,3 +31,32 @@ class CartItem(Base):
     product_id = Column(Integer, ForeignKey("product.product_id", ondelete="CASCADE"), nullable=False)
     quantity = Column(Integer, nullable=False)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
+
+class Order(Base):
+    __tablename__ = "orders"
+
+    order_id = Column(Integer, primary_key=True,index=True)
+    customer_id = Column(UUID, ForeignKey("customer.customer_id", ondelete="CASCADE"), nullable=False)
+    product_id = Column(Integer, ForeignKey("product.product_id", ondelete="CASCADE"), nullable=False)
+    quantity = Column(Integer, nullable=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+
+class  OrderItem(Base):
+    __tablename__ = "order_items"
+
+    order_items_id = Column(Integer, primary_key=True, index=True)
+    order_id = Column(Integer, ForeignKey("order.order_id", ondelete="CASCADE"), nullable=False)
+    product_id = Column(Integer, ForeignKey("prodcut.product_id", ondelete="CASCADE"), nullable=False)
+    quantity = Column(Integer, nullable=False)
+    unit_price = Column(Numeric(10,2), nullable=False)
+    sub_total = Column(Numeric(10,2), nullable=False)
+
+class Payments(Base):
+    __tablename__ = "payments"
+
+    payment_id = Column(Integer, primary_key=True, index=True)
+    order_id = Column(Integer, ForeignKey("order.order_id", ondelete="CASCADE"), nullable=False)
+    payment_method = Column(String(50), nullable=False)
+    payment_status = Column(String(50), nullable=False, default="pending")
+    paid_amount = Column(Numeric(10,2), nullable=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
